c65795932111d7fda7648b6901b0b282c047b2f1
vignesh1999 <vinu6th@gmail.com>
2021-08-01 13:12:34 +0530

    modified auth

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,109 +1,109 @@
-import "./Product_module.css";
-import { FiShoppingCart } from "react-icons/fi";
-import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
-import { useUser } from "../../context/user/userContext";
-import { isWishlisted } from "../../Utils";
-import { Link } from "react-router-dom";
+import './Product_module.css'
+import { FiShoppingCart } from 'react-icons/fi'
+import { BsBookmark, BsFillBookmarkFill } from 'react-icons/bs'
+import { useUser } from '../../context/user/userContext'
+import { isWishlisted } from '../../Utils'
+import { Link, useNavigate } from 'react-router-dom'
 import {
   wishlistAdd,
   wishlistRemove,
-  cartAdd
-} from "../../Services/user.service";
+  cartAdd,
+} from '../../Services/user.service'
 export default function Product(value) {
   const {
     userState: { isLoggedIn, cart, wishlist },
-    userDispatch
-  } = useUser();
-
+    userDispatch,
+  } = useUser()
+  const navigate = useNavigate()
   const wishlistHandler = async (product) => {
     if (!isLoggedIn) {
-      console.log("error");
+      navigate('/login')
     } else {
       const promise = isWishlisted(product, wishlist)
         ? wishlistRemove(product._id)
-        : wishlistAdd(product._id);
+        : wishlistAdd(product._id)
 
       isWishlisted(product, wishlist)
         ? userDispatch({
-            type: "REMOVE_ITEM_FROM_WISHLIST",
-            payload: product
+            type: 'REMOVE_ITEM_FROM_WISHLIST',
+            payload: product,
           })
         : userDispatch({
-            type: "ADD_ITEM_TO_WISHLIST",
-            payload: product
-          });
+            type: 'ADD_ITEM_TO_WISHLIST',
+            payload: product,
+          })
 
-      const response = await promise;
+      const response = await promise
 
       if (!response.success) {
         isWishlisted(product, wishlist)
           ? userDispatch({
-              type: "REMOVE_ITEM_FROM_WISHLIST",
-              payload: product
+              type: 'REMOVE_ITEM_FROM_WISHLIST',
+              payload: product,
             })
           : userDispatch({
-              type: "ADD_ITEM_TO_WISHLIST",
-              payload: product
-            });
+              type: 'ADD_ITEM_TO_WISHLIST',
+              payload: product,
+            })
       }
     }
-  };
+  }
 
   const cartHandler = async (product) => {
     if (!isLoggedIn) {
-      console.log("error");
+      navigate('/login')
     } else {
-      let promise;
+      let promise
       if (isWishlisted(product, cart) === false) {
-        promise = cartAdd(product._id);
+        promise = cartAdd(product._id)
       }
 
       if (isWishlisted(product, cart) === false) {
         userDispatch({
-          type: "ADD_ITEM_TO_CART",
-          payload: product
-        });
+          type: 'ADD_ITEM_TO_CART',
+          payload: product,
+        })
       }
-      const response = await promise;
+      const response = await promise
       if (!response.success) {
         userDispatch({
-          type: "REMOVE_ITEM_FROM_CART",
-          payload: product
-        });
+          type: 'REMOVE_ITEM_FROM_CART',
+          payload: product,
+        })
       }
     }
-  };
+  }
 
   return value.value.map((product) => {
     return (
-      <div key={product.id} className="card2">
-        <div className="card-img2">
-          <img src={product.imageURL} alt="" />
+      <div key={product.id} className='card2'>
+        <div className='card-img2'>
+          <img src={product.imageURL} alt='' />
         </div>
-        <div className="card-desc2">
-          <span className="title2">{product.name}</span>
-          <div className="price-flex">
-            <div className="bold">₹ {product.final_price}</div>
-            <div className="actual-price">₹ {product.original_price}</div>
+        <div className='card-desc2'>
+          <span className='title2'>{product.name}</span>
+          <div className='price-flex'>
+            <div className='bold'>₹ {product.final_price}</div>
+            <div className='actual-price'>₹ {product.original_price}</div>
           </div>
-          <span className="product-desc2">{product.description} </span>
+          <span className='product-desc2'>{product.description} </span>
           {!isWishlisted(product, cart) ? (
             <button
-              className="primary_btn-yellow product-desc"
+              className='primary_btn-yellow product-desc'
               onClick={() => cartHandler(product)}
             >
               Add To Cart
               <FiShoppingCart />
             </button>
           ) : (
-            <button className="primary_btn-yellow product-desc">
-              <Link to="/cart">
+            <button className='primary_btn-yellow product-desc'>
+              <Link to='/cart'>
                 Go to Cart <FiShoppingCart />
               </Link>
             </button>
           )}
           <button
-            className="product-desc primary_btn-yellow"
+            className='product-desc primary_btn-yellow'
             onClick={() => wishlistHandler(product)}
           >
             {isWishlisted(product, wishlist) ? (
@@ -114,6 +114,6 @@ export default function Product(value) {
           </button>
         </div>
       </div>
-    );
-  });
+    )
+  })
 }
