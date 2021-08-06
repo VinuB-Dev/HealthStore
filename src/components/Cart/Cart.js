cfe4fa184ed682d77953afa7c06084af3a5727ae
vignesh1999 <vinu6th@gmail.com>
2021-08-06 18:55:07 +0530

    changed let to const

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,80 +1,80 @@
-import "./Cart_module.css";
-import { useUser } from "../../context/user/userContext";
-import { Link } from "react-router-dom";
+import './Cart_module.css'
+import { useUser } from '../../context/user/userContext'
+import { Link } from 'react-router-dom'
 import {
   incrementQuantity,
   decrementQuantity,
-  cartRemove
-} from "../../Services/user.service";
+  cartRemove,
+} from '../../Services/user.service'
 
 const ShowItem = () => {
   const {
     userState: { cart },
-    userDispatch
-  } = useUser();
+    userDispatch,
+  } = useUser()
 
   const increaseQuantity = async (product) => {
-    let promise = incrementQuantity(product._id);
-    userDispatch({ type: "INCREASE_QUANTITY", payload: product });
-    let response = await promise;
+    const promise = incrementQuantity(product._id)
+    userDispatch({ type: 'INCREASE_QUANTITY', payload: product })
+    const response = await promise
     if (!response.success) {
-      userDispatch({ type: "DECREASE_QUANTITY", payload: product });
+      userDispatch({ type: 'DECREASE_QUANTITY', payload: product })
     }
-  };
+  }
 
   const decreaseQuantity = async (product) => {
-    let promise = decrementQuantity(product._id);
-    userDispatch({ type: "DECREASE_QUANTITY", payload: product });
-    let response = await promise;
+    const promise = decrementQuantity(product._id)
+    userDispatch({ type: 'DECREASE_QUANTITY', payload: product })
+    const response = await promise
     if (!response.success) {
-      userDispatch({ type: "INCREASE_QUANTITY", payload: product });
+      userDispatch({ type: 'INCREASE_QUANTITY', payload: product })
     }
-  };
+  }
 
   const removeFromCart = async (product) => {
-    let promise = cartRemove(product._id);
+    const promise = cartRemove(product._id)
     userDispatch({
-      type: "REMOVE_ITEM_FROM_CART",
-      payload: product
-    });
-    let response = await promise;
+      type: 'REMOVE_ITEM_FROM_CART',
+      payload: product,
+    })
+    const response = await promise
     if (!response.success) {
-      userDispatch({ type: "ADD_ITEM_TO_CART", payload: product });
+      userDispatch({ type: 'ADD_ITEM_TO_CART', payload: product })
     }
-  };
+  }
 
   return cart.map((product) => {
     return (
-      <div key={product._id} className="card">
+      <div key={product._id} className='card'>
         <div>
-          <img className="card-img" src={product.imageURL} alt="" />
+          <img className='card-img' src={product.imageURL} alt='' />
         </div>
-        <div className="card-desc">
-          <span className="title_cart">{product.name}</span>
-          <div className="price-flex1">
+        <div className='card-desc'>
+          <span className='title_cart'>{product.name}</span>
+          <div className='price-flex1'>
             <div>₹ {product.final_price}</div>
           </div>
-          <span className="product-desc1">{product.description} </span>
-          <div className="button-flex">
+          <span className='product-desc1'>{product.description} </span>
+          <div className='button-flex'>
             {product.quantityInCart > 1 && (
               <button
-                className="cart_btn-yellow"
+                className='cart_btn-yellow'
                 onClick={() => decreaseQuantity(product)}
               >
                 -
               </button>
             )}
-            <p style={{ marginTop: "2rem" }}>{product.quantityInCart}</p>
+            <p style={{ marginTop: '2rem' }}>{product.quantityInCart}</p>
             <button
-              className="cart_btn-yellow"
+              className='cart_btn-yellow'
               onClick={() => increaseQuantity(product)}
             >
               +
             </button>
           </div>
-          <div className="button-flex">
+          <div className='button-flex'>
             <button
-              className="cart_btn-yellow"
+              className='cart_btn-yellow'
               onClick={() => removeFromCart(product)}
             >
               Remove
@@ -82,75 +82,75 @@ const ShowItem = () => {
           </div>
         </div>
       </div>
-    );
-  });
-};
+    )
+  })
+}
 
 const CartCard = () => {
   const {
-    userState: { cart }
-  } = useUser();
-  const { TotalPrice, FinalPrice, discount } = addCost(cart);
+    userState: { cart },
+  } = useUser()
+  const { TotalPrice, FinalPrice, discount } = addCost(cart)
   return (
-    <div className="CartCard">
-      <div className="card-heading">Price Details:</div>
-      <div className="card-split">
+    <div className='CartCard'>
+      <div className='card-heading'>Price Details:</div>
+      <div className='card-split'>
         <div>
           <div>Total MRP:</div>
           <div>Total Discount:</div>
-          <div className="final">Final Price:</div>
+          <div className='final'>Final Price:</div>
         </div>
         <div>
           <div>₹ {TotalPrice}</div>
           <div>₹ {discount}</div>
-          <div className="final">₹ {FinalPrice}</div>
+          <div className='final'>₹ {FinalPrice}</div>
         </div>
       </div>
-      <div className="button-flex">
-        <button className="cart_btn-yellow product-desc">Order Now</button>
+      <div className='button-flex'>
+        <button className='cart_btn-yellow product-desc'>Order Now</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const addCost = (cart) => {
   const TotalPrice = cart.reduce(
     (count, item) => count + item.final_price * item.quantityInCart,
     0
-  );
-  const discount = TotalPrice / 10;
-  const FinalPrice = TotalPrice - discount;
-  return { TotalPrice, FinalPrice, discount };
-};
+  )
+  const discount = TotalPrice / 10
+  const FinalPrice = TotalPrice - discount
+  return { TotalPrice, FinalPrice, discount }
+}
 
 export default function Cart() {
   const {
-    userState: { cart }
-  } = useUser();
+    userState: { cart },
+  } = useUser()
 
   return (
     <div>
       {cart.length > 0 && (
         <div>
           <h3>Items in Cart: {cart.length}</h3>
-          <div className="making-grids">
-            <div className="allItems">
+          <div className='making-grids'>
+            <div className='allItems'>
               <ShowItem />
             </div>
-            <div div className="allItems">
+            <div div className='allItems'>
               <CartCard value={cart} />
             </div>
           </div>
         </div>
       )}
       {cart.length === 0 && (
-        <h3 style={{ textAlign: "center", marginLeft: "3rem" }}>
+        <h3 style={{ textAlign: 'center', marginLeft: '3rem' }}>
           No Products in your Cart
-          <button className="primary_btn-yellow">
-            <Link to="/products">Check All Products</Link>
+          <button className='primary_btn-yellow'>
+            <Link to='/products'>Check All Products</Link>
           </button>
         </h3>
       )}
     </div>
-  );
+  )
 }
--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -1,122 +1,122 @@
-import "../Product/Product_module.css";
-import "../../styles.css";
-import { FiShoppingCart } from "react-icons/fi";
-import { RiDeleteBin3Fill } from "react-icons/ri";
-import { useUser } from "../../context/user/userContext";
-import { Link } from "react-router-dom";
-import { isWishlisted } from "../../Utils";
+import '../Product/Product_module.css'
+import '../../styles.css'
+import { FiShoppingCart } from 'react-icons/fi'
+import { RiDeleteBin3Fill } from 'react-icons/ri'
+import { useUser } from '../../context/user/userContext'
+import { Link } from 'react-router-dom'
+import { isWishlisted } from '../../Utils'
 import {
   incrementQuantity,
   cartAdd,
-  wishlistRemove
-} from "../../Services/user.service";
+  wishlistRemove,
+} from '../../Services/user.service'
 const ShowItem = () => {
   const {
     userState: { wishlist, cart },
-    userDispatch
-  } = useUser();
+    userDispatch,
+  } = useUser()
 
   const moveToCart = async (product) => {
-    let promise;
+    let promise
     isWishlisted(product, cart)
       ? (promise = incrementQuantity(product._id))
-      : (promise = cartAdd(product._id));
+      : (promise = cartAdd(product._id))
 
     isWishlisted(product, cart)
       ? userDispatch({
-          type: "INCREASE_QUANTITY",
-          payload: product
+          type: 'INCREASE_QUANTITY',
+          payload: product,
         })
       : userDispatch({
-          type: "ADD_ITEM_TO_CART",
-          payload: product
-        });
+          type: 'ADD_ITEM_TO_CART',
+          payload: product,
+        })
 
-    const response = await promise;
-    const response2 = await wishlistRemove(product._id);
+    const response = await promise
+    const response2 = await wishlistRemove(product._id)
     if (!response.success && !response2.success) {
       userDispatch({
-        type: "REMOVE_ITEM_FROM_CART",
-        payload: product
-      });
+        type: 'REMOVE_ITEM_FROM_CART',
+        payload: product,
+      })
       userDispatch({
-        type: "ADD_ITEM_TO_WISHLIST",
-        payload: product
-      });
+        type: 'ADD_ITEM_TO_WISHLIST',
+        payload: product,
+      })
     }
-  };
+  }
 
   const removeFromWishlist = async (product) => {
     userDispatch({
-      type: "REMOVE_ITEM_FROM_WISHLIST",
-      payload: product
-    });
-    let promise = wishlistRemove(product._id);
-    const response = await promise;
+      type: 'REMOVE_ITEM_FROM_WISHLIST',
+      payload: product,
+    })
+    const promise = wishlistRemove(product._id)
+    const response = await promise
 
     if (!response.success) {
       userDispatch({
-        type: "ADD_ITEM_TO_WISHLIST",
-        payload: product
-      });
+        type: 'ADD_ITEM_TO_WISHLIST',
+        payload: product,
+      })
     }
-  };
+  }
 
   return wishlist.map((product) => {
     return (
-      <div key={product.id} className="card2">
+      <div key={product.id} className='card2'>
         <div>
-          <img className="card-img2" src={product.imageURL} alt="" />
+          <img className='card-img2' src={product.imageURL} alt='' />
         </div>
-        <div className="card-desc2">
-          <span className="title2">{product.name}</span>
-          <div className="price-flex">
-            <div className="bold">₹ {product.final_price}</div>
-            <div className="actual-price">₹ {product.original_price}</div>
+        <div className='card-desc2'>
+          <span className='title2'>{product.name}</span>
+          <div className='price-flex'>
+            <div className='bold'>₹ {product.final_price}</div>
+            <div className='actual-price'>₹ {product.original_price}</div>
           </div>
-          <span className="product-desc2">{product.description} </span>
+          <span className='product-desc2'>{product.description} </span>
           <button
-            className="product-desc primary_btn-yellow"
+            className='product-desc primary_btn-yellow'
             onClick={() => moveToCart(product)}
           >
             Move to Cart <FiShoppingCart />
           </button>
           <button
-            className="primary_btn-yellow product-desc"
+            className='primary_btn-yellow product-desc'
             onClick={() => removeFromWishlist(product)}
           >
             <RiDeleteBin3Fill />
           </button>
         </div>
       </div>
-    );
-  });
-};
+    )
+  })
+}
 
 export default function WishList() {
   const {
-    userState: { wishlist }
-  } = useUser();
+    userState: { wishlist },
+  } = useUser()
   return (
     <div>
       {wishlist.length > 0 && (
         <div>
-          <h3 style={{ textAlign: "center", marginLeft: "3rem" }}>
+          <h3 style={{ textAlign: 'center', marginLeft: '3rem' }}>
             WishList <div>{wishlist.length} Items</div>
           </h3>
-          <div className="grid">
+          <div className='grid'>
             <ShowItem />
-          </div>{" "}
+          </div>{' '}
         </div>
       )}
       {wishlist.length === 0 && (
-        <h3 style={{ textAlign: "center", marginLeft: "3rem" }}>
+        <h3 style={{ textAlign: 'center', marginLeft: '3rem' }}>
           No Products in your Wishlist
-          <button className="primary_btn-yellow product-desc">
-            <Link to="/products">Check All Products</Link>
+          <button className='primary_btn-yellow product-desc'>
+            <Link to='/products'>Check All Products</Link>
           </button>
         </h3>
       )}
     </div>
-  );
+  )
 }
